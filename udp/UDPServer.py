0307cac4fb72801afc64fd49d5ce33a68f4c3bf7
ryan.chen <54sakuramiku@gmail.com>
2022-11-02 20:06:17 +0800

    tcp

--- udp/UDPServer.py
+++ udp/UDPServer.py
@@ -4,7 +4,7 @@ local_ip = "192.168.0.11"
 local_port = 614
 buffer_size = 1024
 
-msg_from_server = "Hello UDP Client----hellohello"
+msg_from_server = "Hello UDP Client"
 bytes_to_send = str.encode(msg_from_server)
 
 UDP_server_socket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
@@ -14,10 +14,12 @@ UDP_server_socket.bind((local_ip, local_port))
 print("UDP server up and listening")
 c = 0
 while(True):
+    msg_from_server = "Hello UDP Client " + str(c)
+    c+=1
+    bytes_to_send = str.encode(msg_from_server)
     bytes_address_pair = UDP_server_socket.recvfrom(buffer_size)
     message = bytes_address_pair[0]
     address = bytes_address_pair[1]
-    bytes_to_send = str.encode(input())
     client_msg = "Message from Client:{}".format(message)
     client_ip = "Client IP Address:{}".format(address)
 
