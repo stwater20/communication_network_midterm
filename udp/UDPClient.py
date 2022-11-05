c74000551358c60a811ba18eb6b66c4f62bd9d0e
rmp871103 <rmpbakuman@gmail.com>
2022-11-05 14:09:44 +0800

    send file version

--- udp/UDPClient.py
+++ udp/UDPClient.py
@@ -1,15 +1,22 @@
 import socket
 
-msg_from_client = "Hello UDP Server"
-
-server_address_port = ("192.168.0.11", 614)
 buffer_size = 1024
 
+host = input("Host: ")
+port = int(input("Port: "))
+server_address_port = (host, port)
+
 UDP_client_socket = socket.socket(family= socket.AF_INET, type = socket.SOCK_DGRAM)
-while(True):
-    msg_from_client = "hello udp server"
-    bytes_to_send = str.encode(msg_from_client)
-    UDP_client_socket.sendto(bytes_to_send, server_address_port)
-    msg_from_server = UDP_client_socket.recvfrom(buffer_size)
-    msg = "Message from Server:{}".format(msg_from_server[0])
-    print(msg)
+
+count = 0
+success = []
+with open('./client/file.txt', 'r', encoding = 'utf-8') as file:
+    new_line = file.readline()
+    while new_line is not None and new_line != '':        
+        msg_from_client = "{} {}".format(count, new_line)
+        bytes_to_send = str.encode(msg_from_client)
+        UDP_client_socket.sendto(bytes_to_send, server_address_port)
+        msg_from_server = UDP_client_socket.recvfrom(buffer_size)
+        success.append(msg_from_server[0])
+        new_line = file.readline()
+        count += 1
--- udp/UDPServer.py
+++ udp/UDPServer.py
@@ -4,6 +4,9 @@ local_ip = "192.168.0.11"
 local_port = 614
 buffer_size = 1024
 
+host = input("Host: ")
+port = int(input("Port: "))
+
 msg_from_server = "Hello UDP Client"
 bytes_to_send = str.encode(msg_from_server)
 
@@ -12,16 +15,18 @@ UDP_server_socket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DG
 UDP_server_socket.bind((local_ip, local_port))
 
 print("UDP server up and listening")
-c = 0
 while(True):
-    msg_from_server = "Hello UDP Client " + str(c)
-    c+=1
-    bytes_to_send = str.encode(msg_from_server)
     bytes_address_pair = UDP_server_socket.recvfrom(buffer_size)
+    
     message = bytes_address_pair[0]
     address = bytes_address_pair[1]
-    bytes_to_send = str.encode("")
     client_msg = "Message from Client:{}".format(message)
     client_ip = "Client IP Address:{}".format(address)
-
+    
+    rec_content = client_msg.split()
+    rec_num = rec_content[0]
+    rec_str = rec_content[1]
+    with open('./server/file.txt', 'a', encoding = 'utf-8') as of:
+        of.write(rec_str + '\n')
+    bytes_to_send = str.encode(rec_num)
     UDP_server_socket.sendto(bytes_to_send, address)
